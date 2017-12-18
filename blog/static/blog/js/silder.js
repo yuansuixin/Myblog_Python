e4887acc758826124bdaf86899e0252b92810530
yuansuixin <cyss428@163.com>
2017-12-18 09:55:25 +0800

    update

---
+++ blog/static/blog/js/silder.js
@@ -0,0 +1,7 @@
+//var obj=null;
+//var As=document.getElementById('nav').getElementsByTagName('a');
+//var As=document.getElementById('nav');
+//obj = As[0];
+//for(i=1;i<As.length;i++){if(window.location.href.indexOf(As[i].href)>=0)
+//obj=As[i];}
+//obj.id='nav_current'
